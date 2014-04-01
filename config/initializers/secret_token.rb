1e186ef0da0259d71860834d58b4f3e0fc04b39e
Helen Hood <helen.c.hood@gmail.com>
2014-03-31 20:38:24 -0400

    Initial commit - new Rails app w/ RSpec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChapstickRatings::Application.config.secret_key_base = '03dc5532811994f2898ac8e8aef09b4556f31e0b46e4feb1ddbc295b95de8e9b3a9d23ca857a2df24e2a77ddf61544ea24f8fa5f369e4cec0b1acc1205cd4c84'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ChapstickRatings::Application.config.session_store :cookie_store, key: '_chapstick_ratings_session'
