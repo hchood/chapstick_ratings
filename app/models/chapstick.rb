33c22b67fe7a1dfb77d5deda6adaa9a3e79effbb
Helen Hood <helen.c.hood@gmail.com>
2014-03-31 20:56:16 -0400

    Add destroy dependencies, has_many through associations

--- app/models/chapstick.rb
+++ app/models/chapstick.rb
@@ -1,5 +1,6 @@
 class Chapstick < ActiveRecord::Base
   validates :brand, presence: true
 
-  has_many :ratings
+  has_many :ratings, dependent: :destroy
+  has_many :raters, through: :ratings, source: :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :ratings
+  has_many :ratings, dependent: :destroy
+  has_many :rated_chapsticks, through: :ratings, source: :chapsticks
 end
