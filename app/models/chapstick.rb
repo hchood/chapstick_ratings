1d81eaafacc7d261664bb4e67f66392b6f50cb38
Helen Hood <helen.c.hood@gmail.com>
2014-03-31 20:51:40 -0400

    Add validations and associations:

--- app/models/chapstick.rb
+++ app/models/chapstick.rb
@@ -1,2 +1,5 @@
 class Chapstick < ActiveRecord::Base
+  validates :brand, presence: true
+
+  has_many :ratings
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,2 +1,8 @@
 class Rating < ActiveRecord::Base
+  validates :user, presence: true
+  validates :chapstick, presence: true
+  validates :stars, presence: true
+
+  belongs_to :user
+  belongs_to :chapstick
 end
