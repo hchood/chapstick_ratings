f07acefc4d196854ff33401ce4b68a1cdbad3fd5
Helen Hood <helen.c.hood@gmail.com>
2014-04-01 17:44:14 -0400

    Load in star ratings for index

--- app/helpers/ratings_helper.rb
+++ app/helpers/ratings_helper.rb
@@ -1,10 +1,22 @@
 module RatingsHelper
   def rating(user, value, chapstick)
-    user_rating_id = chapstick.ratings.where(user: user).take.id || ''
+    user_rating = chapstick.ratings.where(user: user).take || ''
+
+    if user_rating.blank?
+      user_rating_id = ''
+    else
+      user_rating_id = user_rating.id
+    end
     star_id = "chapstick_#{chapstick.id}_#{value}"
 
+    if user_rating.blank? || value > user_rating.stars
+      star_class = "fa fa-star-o star"
+    else
+      star_class = "fa fa-star star"
+    end
+
     content_tag(:a, "",
-      class: 'fa fa-star-o star',
+      class: star_class,
       id: star_id,
       value: value,
       data: {
